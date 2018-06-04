559b95fb374238c0bf31c44b05b7222f9585c794
Plajer <worthplay.pt@onet.pl>
2018-06-04 18:05:14 +0200

    Centered text is a bad idea

--- src/pl/plajer/buildbattle3/utils/MessageUtils.java
+++ src/pl/plajer/buildbattle3/utils/MessageUtils.java
@@ -29,8 +29,6 @@ import org.bukkit.entity.Player;
  */
 public class MessageUtils {
 
-    private final static int CENTER_PX = 154;
-
     public static void thisVersionIsNotSupported() {
         Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "  _   _           _                                                    _                _ ");
         Bukkit.getConsoleSender().sendMessage(ChatColor.RED + " | \\ | |   ___   | |_     ___   _   _   _ __    _ __     ___    _ __  | |_    ___    __| |");
@@ -74,43 +72,6 @@ public class MessageUtils {
         Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + " | |  | | | | | (_| | | |    | (_| | | |_  | | | | | | | (_| |  _   _   _ ");
         Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + " |_|  |_| |_|  \\__, | |_|     \\__,_|  \\__| |_| |_| |_|  \\__, | (_) (_) (_)");
         Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "               |___/                                    |___/             ");
-    }
-
-    public static void sendCenteredMessage(Player player, String message) {
-        if(message == null || message.equals("")) player.sendMessage("");
-        message = ChatColor.translateAlternateColorCodes('&', message);
-
-        int messagePxSize = 0;
-        boolean previousCode = false;
-        boolean isBold = false;
-
-        for(char c : message.toCharArray()) {
-            if(c == '§') {
-                previousCode = true;
-                continue;
-            } else if(previousCode) {
-                previousCode = false;
-                if(c == 'l' || c == 'L') {
-                    isBold = true;
-                    continue;
-                } else isBold = false;
-            } else {
-                DefaultFontInfo dFI = DefaultFontInfo.getDefaultFontInfo(c);
-                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
-                messagePxSize++;
-            }
-        }
-
-        int halvedMessageSize = messagePxSize / 2;
-        int toCompensate = CENTER_PX - halvedMessageSize;
-        int spaceLength = DefaultFontInfo.SPACE.getLength() + 1;
-        int compensated = 0;
-        StringBuilder sb = new StringBuilder();
-        while(compensated < toCompensate) {
-            sb.append(" ");
-            compensated += spaceLength;
-        }
-        player.sendMessage(sb.toString() + message);
     }
 
 }
