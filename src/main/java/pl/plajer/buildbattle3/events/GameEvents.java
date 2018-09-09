518ca1781eebc4aea0fd22ab55662116c7966867
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2018-09-09 12:47:34 +0200

    * Added extra permission "buildbattle.command.bypass" * Added solo and team themes

--- src/main/java/pl/plajer/buildbattle3/events/GameEvents.java
+++ src/main/java/pl/plajer/buildbattle3/events/GameEvents.java
@@ -408,7 +408,7 @@ public class GameEvents implements Listener {
       for (String string : ConfigPreferences.getWhitelistedCommands()) {
         if (event.getMessage().contains(string)) return;
       }
-      if (event.getPlayer().isOp() || event.getPlayer().hasPermission("buildbattle.admin")) return;
+      if (event.getPlayer().isOp() || event.getPlayer().hasPermission("buildbattle.admin") || event.getPlayer().hasPermission("buildbattle.command.bypass"))return;
       if (event.getMessage().contains("leave") || event.getMessage().contains("stats")) return;
       event.setCancelled(true);
       event.getPlayer().sendMessage(ChatManager.colorMessage("In-Game.Only-Command-Ingame-Is-Leave"));
