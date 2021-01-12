9952309d96f53d2709507df447e452b335cbea7a
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2021-01-12 23:38:04 +0100

    Updated Copyright

--- src/main/java/plugily/projects/buildbattle/events/ChatEvents.java
+++ src/main/java/plugily/projects/buildbattle/events/ChatEvents.java
@@ -1,6 +1,7 @@
 /*
+ *
  * BuildBattle - Ultimate building competition minigame
- * Copyright (C) 2020 Plugily Projects - maintained by Tigerpanzer_02, 2Wild4You and contributors
+ * Copyright (C) 2021 Plugily Projects - maintained by Tigerpanzer_02, 2Wild4You and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package plugily.projects.buildbattle.events;
