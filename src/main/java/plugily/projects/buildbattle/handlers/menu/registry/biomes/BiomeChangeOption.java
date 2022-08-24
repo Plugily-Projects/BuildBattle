36c51abf9a769c8bcd47deaa8c61d49cc69557b8
montlikadani <montlikada@gmail.com>
2022-08-24 12:22:57 +0200

    Make sure to perform this refresh chunk in Paper

--- src/main/java/plugily/projects/buildbattle/handlers/menu/registry/biomes/BiomeChangeOption.java
+++ src/main/java/plugily/projects/buildbattle/handlers/menu/registry/biomes/BiomeChangeOption.java
@@ -34,6 +34,7 @@ import plugily.projects.buildbattle.handlers.misc.ChunkManager;
 import plugily.projects.minigamesbox.classic.handlers.language.MessageBuilder;
 import plugily.projects.minigamesbox.classic.utils.helper.ItemBuilder;
 import plugily.projects.minigamesbox.classic.utils.version.ServerVersion;
+import plugily.projects.minigamesbox.classic.utils.version.VersionUtils;
 import plugily.projects.minigamesbox.classic.utils.version.xseries.XMaterial;
 
 /**
@@ -94,7 +95,7 @@ public class BiomeChangeOption {
         }
 
         for(Chunk chunk : plot.getCuboid().chunkList()) {
-          if(ServerVersion.Version.isCurrentEqualOrHigher(ServerVersion.Version.v1_18_R1)) {
+          if(ServerVersion.Version.isCurrentEqualOrHigher(ServerVersion.Version.v1_18_R1) && VersionUtils.isPaper()) {
             chunk.getWorld().refreshChunk(chunk.getX(), chunk.getZ());
             continue;
           }
