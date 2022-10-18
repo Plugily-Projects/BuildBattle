a9335aef33d57c0d61a01fcff1a9c8991550845c
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2022-10-18 23:09:07 +0200

    Fixed placeholder infinite loop at game summary

--- src/main/java/plugily/projects/buildbattle/boot/PlaceholderInitializer.java
+++ src/main/java/plugily/projects/buildbattle/boot/PlaceholderInitializer.java
@@ -205,12 +205,12 @@ public class PlaceholderInitializer {
             }
             StringBuilder members = new StringBuilder();
             List<Player> players = ((BuildArena) pluginArena).getTopList().get(number);
-            if(players != null) {
+            if(!players.isEmpty()) {
               players.forEach(p -> members.append(p.getName()).append(" & "));
               members.delete(members.length() - 3, members.length());
               return new MessageBuilder(members.toString()).build();
             }
-            return null;
+            return "-";
           }
           if(pluginArena instanceof GuessArena) {
             if(pluginArena.getArenaInGameState() != BaseArena.ArenaInGameState.PLOT_VOTING && pluginArena.getArenaState() != ArenaState.ENDING) {
@@ -243,7 +243,11 @@ public class PlaceholderInitializer {
             if(pluginArena.getArenaInGameState() != BaseArena.ArenaInGameState.PLOT_VOTING && pluginArena.getArenaState() != ArenaState.ENDING) {
               return null;
             }
-            return String.valueOf(((BuildArena) pluginArena).getPlotFromPlayer(((BuildArena) pluginArena).getTopList().get(number).get(0)).getPoints());
+            List<Player> players = ((BuildArena) pluginArena).getTopList().get(number);
+            if(!players.isEmpty()) {
+              return String.valueOf(((BuildArena) pluginArena).getPlotFromPlayer(players.get(0)).getPoints());
+            }
+            return "-";
           }
           if(pluginArena instanceof GuessArena) {
             if(pluginArena.getArenaInGameState() != BaseArena.ArenaInGameState.PLOT_VOTING && pluginArena.getArenaState() != ArenaState.ENDING) {
@@ -449,6 +453,9 @@ public class PlaceholderInitializer {
           return null;
         }
         int places = pluginArena.getPlayersLeft().size();
+        if(pluginArena instanceof BuildArena) {
+          places = ((BuildArena) pluginArena).getTopList().size();
+        }
         if(places > 16) {
           places = 16;
         }
