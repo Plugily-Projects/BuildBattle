60b43e1def735bae698107bd7fabf4b409bd6c04
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2023-08-09 20:26:58 +0200

    Moved locales to remote

--- src/main/java/plugily/projects/buildbattle/boot/MessageInitializer.java
+++ src/main/java/plugily/projects/buildbattle/boot/MessageInitializer.java
@@ -38,7 +38,6 @@ public class MessageInitializer {
 
   public MessageInitializer(Main plugin) {
     this.plugin = plugin;
-    registerLocales();
   }
 
   public void registerMessages() {
@@ -171,21 +170,4 @@ public class MessageInitializer {
     messageManager.registerMessage("LEADERBOARD_STATISTICS_SUPER_VOTES", new Message("Leaderboard.Statistics.Super-Votes", ""));
   }
 
-  private void registerLocales() {
-    Arrays.asList(new Locale("Chinese (Simplified)", "简体中文", "zh_CN", "POEditor contributors", Arrays.asList("简体中文", "中文", "chinese", "chinese_simplified", "cn")),
-            new Locale("Czech", "Český", "cs_CZ", "POEditor contributors", Arrays.asList("czech", "cesky", "český", "cs")),
-            new Locale("Dutch", "Nederlands", "nl_NL", "POEditor contributors", Arrays.asList("dutch", "nederlands", "nl")),
-            new Locale("English", "English", "en_GB", "Tigerpanzer_02", Arrays.asList("default", "english", "en")),
-            new Locale("French", "Français", "fr_FR", "POEditor contributors", Arrays.asList("french", "francais", "français", "fr")),
-            new Locale("French (BE)", "Belgian French", "fr_BE", "POEditor contributors", Arrays.asList("belgiumfrench", "fr-be", "fr_be")),
-            new Locale("German", "Deutsch", "de_DE", "Tigerkatze and POEditor contributors", Arrays.asList("deutsch", "german", "de")),
-            new Locale("Italian", "Italiano", "it_IT", "POEditor contributors", Arrays.asList("italian", "italiano", "it")),
-            new Locale("Persian", "Persian", "fa_FA", "POEditor contributors", Arrays.asList("persian", "fa", "fa-FA")),
-            new Locale("Polish", "Polski", "pl_PL", "Plajer", Arrays.asList("polish", "polski", "pl")),
-            new Locale("Portuguese (BR)", "Português Brasileiro", "pt_BR", "POEditor contributors", Arrays.asList("brazilian", "brasil", "brasileiro", "pt-br", "pt_br")),
-            new Locale("Russian", "Pусский", "ru_RU", "POEditor contributors", Arrays.asList("russian", "pусский", "pyccknn", "russkiy", "ru")),
-            new Locale("Spanish", "Español", "es_ES", "POEditor contributors", Arrays.asList("spanish", "espanol", "español", "es")),
-            new Locale("Turkish", "Türk", "tr_TR", "POEditor contributors", Arrays.asList("turkish", "turk", "türk", "tr")))
-        .forEach(LocaleRegistry::registerLocale);
-  }
 }
